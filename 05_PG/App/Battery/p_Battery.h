baa2b1ca5465a007c6cd0e1f4206583d25e5174c
Masaaki Nemoto <eequalmgh@gmail.com>
2019-09-25 21:32:33 +0900

    Create p_Battery.h
    バッテリーの残量チェックの処理を追加した。

---
+++ 05_PG/App/Battery/p_Battery.h
@@ -0,0 +1,42 @@
+/*
+ * p_Battery.h
+ *
+ *  Created on: Sep 22, 2019
+ *      Author: Akimoto Masane
+ */
+
+#ifndef BATTERY_P_BATTERY_H_
+#define BATTERY_P_BATTERY_H_
+
+
+extern ADC_HandleTypeDef hadc1;
+
+
+/* Exported types ------------------------------------------------------------*/
+#define BATTERY_MOVING_AVERAGE_COUNT	(4u)
+#define BATTERY_RC_FILTER				(100u)
+#define BATTERY_FILTER_COEFFICIENT		(10u)
+
+#define BATTERY_AD_CONVERSION_TIMEOUT	(10u)
+#define BATTERY_AD_CONVERSION_INTERVAL	(1000u)
+
+#define BATTERY_AD_MAX_VALUE	(0x0FFFu)
+
+
+typedef struct {
+
+	uint32_t battery;
+	uint32_t output;
+	uint32_t index;
+	uint32_t buffer[BATTERY_MOVING_AVERAGE_COUNT];
+
+} stBatteryBuffer;
+
+
+/* Exported functions --------------------------------------------------------*/
+static void initBatteryTask(void);
+static uint32_t exeRcFilter(const uint32_t preValue, const uint32_t value);
+static uint32_t exeMovingAverage(const uint32_t size, const uint32_t * const pBuffer);
+
+
+#endif /* BATTERY_P_BATTERY_H_ */
