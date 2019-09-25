fe70c86993746b50efcb30369f3f0db41989273a
Masaaki Nemoto <eequalmgh@gmail.com>
2019-09-25 21:32:39 +0900

    Create Battery.h
    バッテリーの残量チェックの処理を追加した。

---
+++ 05_PG/App/Inc/Battery.h
@@ -0,0 +1,16 @@
+/*
+ * Battery.h
+ *
+ *  Created on: Sep 22, 2019
+ *      Author: Akimoto Masane
+ */
+
+#ifndef INC_BATTERY_H_
+#define INC_BATTERY_H_
+
+
+/* Exported functions --------------------------------------------------------*/
+void BatteryTask(void);
+
+
+#endif /* INC_BATTERY_H_ */
