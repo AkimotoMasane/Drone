e3a660e14edece918d39dafd4df92f5752d0d462
Masaaki Nemoto <eequalmgh@gmail.com>
2020-02-17 09:55:53 +0900

    Create Lsm6dsl.h
    LSM6DSLとのSPI通信処理を修正した。

---
+++ 05_PG/App/Inc/Lsm6dsl.h
@@ -0,0 +1,15 @@
+/*
+ * Lsm6dsl.h
+ *
+ *  Created on: Oct 14, 2019
+ *      Author: Akimoto Masane
+ */
+
+#ifndef INC_LSM6DSL_H_
+#define INC_LSM6DSL_H_
+
+
+void Lsm6dslTask(void);
+
+
+#endif /* INC_LSM6DSL_H_ */
